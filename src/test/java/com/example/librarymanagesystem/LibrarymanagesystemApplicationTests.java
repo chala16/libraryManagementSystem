04973895ca1c52e0f0478ed182377360feea9f8e
chala16 <it22338952@my.sliit.lk>
2025-07-10 01:55:51 +0530

    project setup

---
+++ src/test/java/com/example/librarymanagesystem/LibrarymanagesystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.librarymanagesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarymanagesystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
